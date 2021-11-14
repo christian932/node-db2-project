0a351679aee59234dc40dc1eb3cc24f2ec398056
christian932 <christianfarnsworth@gmail.com>
2021-11-14 13:31:46 -0800

    done

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,23 @@
-const getAll = () => {
-  // DO YOUR MAGIC
-}
+const db = require("../../data/db-config");
 
-const getById = () => {
-  // DO YOUR MAGIC
-}
+const getAll = async () => {
+  const result = await db("cars");
+  return result;
+};
 
-const create = () => {
-  // DO YOUR MAGIC
-}
+const getById = async () => {
+  const result = await db("cars").where("id", id).first();
+  return result;
+};
+
+const create = async () => {
+  const [id] = await db("cars").insert(car);
+  const post = await getById(id);
+  return post;
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
